da9fd5c9d9b34d735f6652efe56dc215d248c610
Sasikala21 <sasisakth08@gmail.com>
2024-02-10 22:21:02 +0530

    postman collection added

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import UserController from '../controllers/userController';
 import upload from '../middlewares/upload';
-// import checkDuplicateUsernameOrEmail from '../middlewares/checkDuplicateUsernameOrEmail';
 import authJWT from '../middlewares/authJWT';
 import roleAuth from '../middlewares/roleAuth';
 
@@ -9,11 +8,11 @@ const router = express.Router();
 
 router.get('/list', authJWT.verifyToken, roleAuth.isAdmin, UserController.getAllUsers);
 router.get('/currentUser', authJWT.verifyToken, UserController.getCurrentUser);
-router.route('/:id').get(authJWT.verifyToken, roleAuth.isAdmin, UserController.getUserById).put(authJWT.verifyToken, upload, UserController.createOrUpdateUser);
+router.route('/:id').get(authJWT.verifyToken, UserController.getUserById).put(authJWT.verifyToken, upload, UserController.createOrUpdateUser);
 router.post('/', authJWT.verifyToken, roleAuth.isAdmin, upload, UserController.createOrUpdateUser);
 router.patch('/status/:id', authJWT.verifyToken, roleAuth.isAdmin, UserController.updateStatusUser);
 router.delete('/:id', authJWT.verifyToken, roleAuth.isAdmin, UserController.deleteUser);
-router.delete('/image/:id', authJWT.verifyToken, roleAuth.isAdmin, UserController.deleteImage);
+router.delete('/image/:id', authJWT.verifyToken, UserController.deleteImage);
 router.post('/changepassword', authJWT.verifyToken, UserController.changePassword);
 
 export default router;
